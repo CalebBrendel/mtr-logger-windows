c1dd93d954359818309850f179b56d54aee16db5
Caleb Brendel <109250665+JumperTheHero@users.noreply.github.com>
2024-09-19 19:29:16 -0500

    Update staging.bat - i'm dumb part 2

--- Bin/MTR/Application/Bat_Files/staging.bat
+++ Bin/MTR/Application/Bat_Files/staging.bat
@@ -10,6 +10,7 @@ mkdir C:\Bin
 mkdir C:\Bin\MTR
 mkdir C:\Bin\MTR\Application
 mkdir C:\Bin\MTR\Logs
+mkdir C:\Bin\MTR\Logs\Archive
 mkdir C:\Bin\MTR\Application\Bat_Files
 mkdir C:\Bin\MTR\Application\MTR
 mkdir C:\Bin\MTR\Application\Task_Scheduler
