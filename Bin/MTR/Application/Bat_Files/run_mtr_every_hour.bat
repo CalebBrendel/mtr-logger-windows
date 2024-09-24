52340081a71805f0ddc4f27dfa8ce503521d6d64
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2024-09-24 14:17:56 -0500

    Updated the amount of cycles ran during each hour.
    This will prevent the task from failing on older/slower hardware when starting at the top of the hour.

--- Bin/MTR/Application/Bat_Files/run_mtr_every_hour.bat
+++ Bin/MTR/Application/Bat_Files/run_mtr_every_hour.bat
@@ -1,3 +1,3 @@
 @echo off
 cd "C:\Bin\MTR\Application\MTR"
-mtr.exe -c 3600 -i 0.2 -t 1 -o "LS ABW G" -f "C:\Bin\MTR\Logs\%DATE:~4,2%-%DATE:~7,2%-%DATE:~-4%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%.txt" -r 8.8.8.8
+mtr.exe -c 3595 -i 0.2 -t 1 -o "LS ABW G" -f "C:\Bin\MTR\Logs\%DATE:~4,2%-%DATE:~7,2%-%DATE:~-4%-%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%.txt" -r 8.8.8.8
